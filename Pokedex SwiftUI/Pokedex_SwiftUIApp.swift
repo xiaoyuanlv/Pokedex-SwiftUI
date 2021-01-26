06756a3e807b1d4111ddeaefd1fccf6192ea33d8
Xiao Yuan Lv <xiaoyuanlv.31@gmail.com>
2021-01-26 13:20:40 +0630

    Initial Commit

---
+++ Pokedex SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex SwiftUI
+//
+//  Created by Xiao Yuan on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
